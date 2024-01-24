367f6ce2a7c5d41be57473b6341bd13085a78829
zhj <zhuhaojie789@126.com>
2024-01-24 14:51:11 +0800

    init

---
+++ src/main/webapp/js/js.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+$(function(){
+	$(".kaiList tr:lt(3)").css("color","#F99500");
+	$(".banner").click(function(){
+		$(".allBack").show();
+		$(".fuwuqu").show();
+		})
+	$(".allBack,.close").click(function(){
+		$(".allBack").hide();
+		$(".fuwuqu").hide();
+		})
+		
+	})
+	
+	
+	
